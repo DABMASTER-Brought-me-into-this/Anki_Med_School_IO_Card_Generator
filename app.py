cd0317e7d98c3bd10fb38c77c99cb78fcbe959d7
ItzTofuAgain <78882728+DABMASTER-Brought-me-into-this@users.noreply.github.com>
2026-02-19 20:11:34 -0800

    Update app.py

--- app.py
+++ app.py
@@ -54,3 +54,30 @@ if uploaded_file and api_key and st.button("Generate Deck"):
             except Exception as e:
                 st.error(f"An error occurred: {e}")
 
+
+# Instructions
+with st.expander("How to import this into Anki?"):
+    st.markdown(r"""
+    1. Go to your download folder and unzip your file. The unzipped file should be a folder that contains text files, image files, and a csv. 
+    2. If your file does not contain one of those items, refresh the website and recreate your zip file. 
+    3. Select all the images in the folder, and press 
+     Mac: 'command + x' 
+     Windows/Linux: 'control + x'  
+    4. In your files, locate the Anki App
+    5. Locate the media folder within your Anki App. It is likely stored in these file locations: 
+     Windows: '%APPDATA%\Anki2\<Your Profile Name>\collection.media' 
+     Mac: '~/Library/Application Support/Anki2/<Your Profile Name>/collection.media' 
+     Linux: '~/.local/share/Anki2/<Your Profile Name>/collection.media' 
+    If you did not create a profile name, the <Your Profile Name> is likely User 1
+    6. Press 
+     Mac: 'command + v' 
+     Windows/Linux: 'control + v'
+    7. Now open the Anki
+    8. Click import on the top right corner
+    9. Find the .csv file in the folder you downloaded, and click on it
+    10. Finally, set the type to 'Basic'(The result will be same as if they were Image Occlusion Cards) 
+    Choose the Deck where you want this to live 
+    Find the 'Field Separator' and choose Commas 
+    
+    ###Congrats you have a new Med School Anki Deck! 
+    """)
